55b3f52ee9856006bf05ac7e04bcf9ec578e5574
olgaaktas <binpolyglotte@gmail.com>
2020-12-30 17:02:09 +0300

    added landing and register screens

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { NavigationContainer } from 'react-navigation/native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import LandingScreen from './components/auth/Landing' 
 
 const Stack = createStackNavigator()
 
 export default function App() {
   return (
-  <NavigationContainer></NavigationContainer>
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName="Landing">
+      <Stack.Screen name="Landing" component={LandingScreen} options={{headerShown: false}}></Stack.Screen>
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
--- components/auth/Landing.js
+++ components/auth/Landing.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, Button } from 'react-native'
 
-export default function Landing() {
+export default function Landing({navigation}) {
     return (
-        <div>
-            
-        </div>
+        <View style={{flex:1, justifyContent: 'center'}}>
+            <Button title="Register" onPress={()=> navigation.navigate("Register")}></Button>
+            <Button title="Login" onPress={()=> navigation.navigate("Login")}></Button>
+        </View>
     )
 }
