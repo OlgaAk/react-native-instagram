c8dd139fd681b90153f2499a8ee4216ade169f93
olgaaktas <binpolyglotte@gmail.com>
2020-12-30 17:42:47 +0300

    added firebase and env

--- App.js
+++ App.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import * as firebase from 'firebase'
+import { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_PROJECT_ID, 
+  FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_APP_ID } from '@env'
+
+var firebaseConfig = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+};
+
+if(firebase.apps.length === 0){
+  firebase.initializeApp(firebaseConfig)
+}
+
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import LandingScreen from './components/auth/Landing' 
+import RegisterScreen from './components/auth/Register'
+
 
 const Stack = createStackNavigator()
 
@@ -10,6 +29,7 @@ export default function App() {
   <NavigationContainer>
     <Stack.Navigator initialRouteName="Landing">
       <Stack.Screen name="Landing" component={LandingScreen} options={{headerShown: false}}></Stack.Screen>
+      <Stack.Screen name="Register" component={RegisterScreen} ></Stack.Screen>
     </Stack.Navigator>
   </NavigationContainer>
   );
