7ac10deef216c68aff1159f29212aec7c5bd9291
olgaaktas <binpolyglotte@gmail.com>
2021-01-04 18:42:56 +0300

    added flatlist with user images to profile screen

--- components/main/Profile.js
+++ components/main/Profile.js
@@ -2,23 +2,46 @@ import React from "react";
 import { StyleSheet, View, Text, Image, FlatList } from "react-native";
 import { connect } from "react-redux";
 
-function Profile(props) {
+const Profile = (props) => {
     const { currentUser, posts } = props;
-    return (
-        <View style={styles.container}>
-            <Text>{currentUser.name}</Text>
-            <Text>{posts[0].imageUrl}</Text>
-            <Image style={styles.image} source={{ uri: posts[0].imageUrl }} />
+    const renderItem = ({ item }) => (
+        <View style={styles.containerImage}>
+            <Image style={styles.image} source={{ uri: item.imageUrl }} />
         </View>
     );
-}
+    return (
+        <View style={styles.container}>
+            <View style={styles.containerInfo}>
+                <Text>{currentUser.name}</Text>
+                <Text>{currentUser.email}</Text>
+            </View>
+            <View style={styles.containerGallery}>
+                <FlatList
+                    numColumns={3}
+                    horizontal={false}
+                    data={posts}
+                    renderItem={renderItem}
+                />
+            </View>
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         marginTop: 40
     },
-    image: { flex: 0.2 }
+    containerInfo: {
+        margin: 10
+    },
+    containerGallery: {
+        flex: 1
+    },
+    containerImage: {
+        flex: 1 / 3
+    },
+    image: { flex: 1, aspectRatio: 1 / 1 }
 });
 
 const mapStateToProps = (store) => ({
